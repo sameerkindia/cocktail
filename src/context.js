5ab1a4861df1878d6f340ec5753b43424e320d40
sameerkindia <sameerkhanmm355@gmail.com>
2023-09-20 17:05:59 +0530

    first commit

---
+++ src/context.js
@@ -0,0 +1,62 @@
+import React, { useState, useContext, useEffect } from "react";
+import { useCallback } from "react";
+
+const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  const [loading, setLoading] = useState(true);
+  const [searchTerm, setSearchTerm] = useState("lassi");
+  const [cocktails, setCocktails] = useState([]);
+
+  const fetchingDrinks = useCallback(async () => {
+    setLoading(true);
+    try {
+      const res = await fetch(`${url}${searchTerm}`);
+      const data = await res.json();
+      const { drinks } = data;
+
+      if (drinks) {
+        const newCocktails = drinks.map((item) => {
+          const { idDrink, strDrink, strDrinkThumb, strAlcoholic, strGlass } =
+            item;
+
+          return {
+            id: idDrink,
+            name: strDrink,
+            image: strDrinkThumb,
+            info: strAlcoholic,
+            glass: strGlass,
+          };
+        });
+
+        setCocktails(newCocktails);
+        setLoading(false);
+      } else {
+        setCocktails([]);
+
+        setLoading(false);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }, [searchTerm]);
+
+  useEffect(() => {
+    fetchingDrinks();
+
+    return () => fetchingDrinks();
+  }, [searchTerm, fetchingDrinks]);
+
+  return (
+    <AppContext.Provider value={{ loading, cocktails, setSearchTerm }}>
+      {children}
+    </AppContext.Provider>
+  );
+};
+// make sure use
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+        itaque animi quidem asperiores dolore repudiandae sequi numquam modi?
+        Architecto suscipit quae tempora et neque ratione fugiat quidem soluta
+        placeat exercitationem?
+      </p>
+    </section>
+  );
+};
+
+export default About;
